43390d6c7cfc59b9dbdfd36f2dd1297fd166d6cc
alana3098 <57573144+alana3098@users.noreply.github.com>
2022-08-08 16:25:19 -0400

    Add files via upload

---
+++ Button-clicker/buttonClicker.js
@@ -0,0 +1,15 @@
+function logout(element) {
+    element.innerText="Logout";
+}
+logout(element);
+function hide(button) {
+    button.remove();
+}
+// likes count
+  //  var clicks = 0;
+//function onClick() 
+//{
+   // clicks += 1;
+ //   document.getElementById("liked").innerHTML = clicks;
+    
+//}
